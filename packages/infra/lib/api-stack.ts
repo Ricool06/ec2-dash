5cfc1523ffeb544607864894ec84861eb42f6d1c
Ricool06 <Ricool06@users.noreply.github.com>
2019-11-03 19:42:21 +0000

    Write infra code for ec2-fetcher lambda

--- packages/infra/lib/api-stack.ts
+++ packages/infra/lib/api-stack.ts
@@ -1,4 +1,5 @@
-import { AuthorizationType, LambdaIntegration, RestApi, CfnAuthorizer } from '@aws-cdk/aws-apigateway';
+import { AuthorizationType, CfnAuthorizer, LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
 import { Code, Function as LambdaFunction, Runtime } from '@aws-cdk/aws-lambda';
 import { Construct, Fn, Stack, StackProps } from '@aws-cdk/core';
 import * as path from 'path';
@@ -7,19 +8,23 @@ export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps, deploymentStage: string) {
     super(scope, id, props);
 
-    const cognitoUserPoolId = Fn.importValue(`${deploymentStage}CognitoUserPoolId`);
     const cognitoUserPoolArn = Fn.importValue(`${deploymentStage}CognitoUserPoolArn`);
 
     const getInstancesLambda = new LambdaFunction(this, 'get-instances', {
       code: Code.fromAsset(path.join(__dirname, '../../ec2-fetcher/.serverless/ec2-fetcher.zip')),
-      handler: 'handler.handler',
+      handler: 'src/handler.handler',
       runtime: Runtime.NODEJS_10_X,
     });
+
+    getInstancesLambda.addToRolePolicy(new PolicyStatement({
+      actions: ['ec2:DescribeInstances'],
+      resources: ['*'],
+    }));
 
     const restApi = new RestApi(this, 'ec2-dash-api');
 
     const apiAuthorizer = new CfnAuthorizer(this, 'ec2-dash-cognito-authorizer', {
-      identitySource: 'method.request.header.AuthToken',
+      identitySource: 'method.request.header.Authorization',
       name: 'ec2-dash-cognito-authorizer',
       providerArns: [cognitoUserPoolArn],
       restApiId:  restApi.restApiId,
